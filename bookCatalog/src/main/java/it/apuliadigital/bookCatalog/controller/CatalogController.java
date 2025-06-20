6c56d305cd533a30462d6f05cc415cddf2c91b61
Flavio <flaviostincone711@gmail.com>
2025-06-20 12:26:21 +0200

    work in progress

--- bookCatalog/src/main/java/it/apuliadigital/bookCatalog/controller/CatalogController.java
+++ bookCatalog/src/main/java/it/apuliadigital/bookCatalog/controller/CatalogController.java
@@ -1,5 +1,26 @@
 package it.apuliadigital.bookCatalog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.apuliadigital.bookCatalog.model.Book;
+import it.apuliadigital.bookCatalog.service.CatalogService;
+
+@RestController
 public class CatalogController {
+
+    @Autowired
+    private CatalogService service;
+    
+    //POST /catalog
+    @PostMapping("/catalog")
+    public ResponseEntity<Book> catalog(Book book)
+    {
+        Book newBook = service.addBook(book);
+        
+        return ResponseEntity.ok(book);
+    }
     
 }
